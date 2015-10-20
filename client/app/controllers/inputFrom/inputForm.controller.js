8409b592b95de4b48222c53a094c1e35dfb6cd8a
Blackstock <charlieblackstock@gmail.com>
2015-10-20 11:57:37 -0600

    cleaned up investment table compononent

---
+++ client/app/controllers/inputFrom/inputForm.controller.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  angular.module('app.controllers.inputForm')
+  .controller('InputFormController', function($scope, $http, myfactory){
+
+    $scope.initalAmt = 5000;
+
+  });
+
+})();
--- client/app/controllers/investmentTable/investmentTable.controller.js
+++ client/app/controllers/investmentTable/investmentTable.controller.js
@@ -3,13 +3,11 @@
 
   angular.module('app.controllers.investmentTable')
   .controller('InvestmentTableController', function($scope, $http, myfactory){
-    // calling the api, grabbing the value for USD, appending it to the dom
-
-    $scope.initalAmt = 5000;
-
+    
     //set the funcitons from the factory
     $scope.newAmt = myfactory.calcNewAmt;
     $scope.profit = myfactory.calcProfit;
+
   });
 
 })();
